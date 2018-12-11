4aa31eb41f22f15f1ed6aaf5ffee2dcfaecc42b3
Rafael Ulises Mendoza Reyes <rumendoza@localhost.localdomain>
2018-12-11 10:34:06 -0600

    first commit

--- config.js
+++ config.js
@@ -1,13 +1,17 @@
 "use strict";
 
-require('dotenv').config();
-
 var config = {
-  NEXMO_API_KEY: process.env['NEXMO_API_KEY'],
-  NEXMO_API_SECRET: process.env['NEXMO_API_SECRET'],
-  NEXMO_DEBUG: process.env['NEXMO_DEBUG'],
-  NEXMO_APP_ID: process.env['NEXMO_APP_ID'],
-  PROVISIONED_NUMBERS: process.env['PROVISIONED_NUMBERS']? JSON.parse(process.env['PROVISIONED_NUMBERS']) : []
+  NEXMO_API_KEY: '1e305e74',
+  NEXMO_API_SECRET: "UIE4CWMJZKZcRXXS",
+  NEXMO_DEBUG: "",
+  NEXMO_APP_ID: "7f048630-3be3-4357-a3bd-ed82dbd1c33f",
+  PROVISIONED_NUMBERS: [{
+      msisdn: '525590909090',
+      country: 'MX'
+    },{
+      msisdn: '525520202020',
+      country: 'MX'
+    }]
 };
 
 module.exports = config;
